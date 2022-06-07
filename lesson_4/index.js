9e3169630873b1880febe456d18f709856b594ca
Максим Сергеев <msergeev@MacBook-Pro-Maksim.local>
2022-06-07 15:45:47 +0300

    lessons 4, lessons 5

---
+++ lesson_4/index.js
@@ -0,0 +1,78 @@
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?");
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+
+    while (isNaN(money) || money === null || money === "") {
+        money = +prompt("Ваш бюджет на месяц?");
+    }
+}
+start();
+
+// console.log(money);
+// console.log(time);
+
+// let answer1 = prompt("Введите обязательную статью расходов в этом месяце");
+// let answer2 = prompt("Во сколько обойдется?");
+// let answer3 = prompt("Введите обязательную статью расходов в этом месяце");
+// let answer4 = prompt("Во сколько обойдется?");
+
+const appData = {
+    budget: money,
+    timeData: time,
+    expences: {},
+    optionalExpences: {},
+    income: [],
+    savings: true,
+    chooseExpences: function() {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt("Введите обязательную статью расходов в этом месяце");
+            let b = prompt("Во сколько обойдется?");
+            if ((typeof(a)) === 'string' && ((typeof(a)) != null) && ((typeof(b)) != null) && a != '' && b != '' && a.length < 50) {
+                console.log("done");
+                appData.expences[a] = b;
+            } else {
+                console.log("Данные не были сохранены, некорректный ввод!");
+                i--;
+            }
+        };
+    },
+    detectDayBudget: function() {
+        appData.moneyPerDay = (appData.budget / 30).toFixed();
+        alert("Ежедневный бюджет: " + appData.moneyPerDay);
+    },
+    detectLevel: function() {
+        if (appData.moneyPerDay < 100) {
+            console.log("Минимальный уровень достатка");
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+            console.log("Средний уровень достатка");
+        } else if (appData.moneyPerDay > 2000) {
+            console.log("Высокий уровень достатка");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+    checkSavings: function() {
+        if (appData.savings === true) {
+            let save = +prompt("Какова сумма накоплений?");
+            let percent = +prompt("Под какой процент?");
+
+            appData.monthIncome = save / 100 / 12 * percent;
+            alert("Ваш доход в месяц с вашего депозита:" + appData.monthIncome);
+        }
+    },
+    chooseOptExpences: function() {
+        for (let i = 0; i < 3; i++) {
+            let optExpencesName = prompt("Введите необязательную статью расходов");
+            let optExpencesValue = prompt("Во сколько обойдется?");
+            appData.optionalExpences[optExpencesName] = optExpencesValue;
+        }
+    },
+    chooseIncome: function() {
+        let items = prompt("Что принесет дополнительный доход? (Перечислите через запятую)", "");
+        appData.income = items.split(', ');
+        appData.income.push(prompt("Может что-то еще?"));
+        appData.income.sort();
+    }
+}
---
+++ lesson_5/js/script.js
@@ -0,0 +1,29 @@
+let menu = document.querySelector('.menu');
+let menuItem = document.getElementsByClassName('menu-item');
+console.log(menuItem);
+
+let li = document.createElement('li')
+
+//Добпаление 5го пункта и смена местами 2го и 3го пунктов
+li.classList.add('menu-item');
+li.innerHTML = 'Пятый пункт';
+menu.appendChild(li);
+
+menu.insertBefore(menuItem[1], menuItem[3]);
+
+//смена заголовка
+let title = document.getElementById('title');
+console.log(title);
+title.innerHTML = "Мы продаем только подлинную технику Apple"
+
+//замена картинки
+let bg = './img/apple_true.jpg';
+document.body.style.background = `url(${bg}) no-repeat center`;
+
+//удаление рекламы
+document.querySelector('.column > .adv').remove();
+
+//задаем вопрос
+let question = prompt("Как вы относитесь к технике Apple?")
+let promptOut = document.getElementById('prompt')
+promptOut.innerHTML = question;
