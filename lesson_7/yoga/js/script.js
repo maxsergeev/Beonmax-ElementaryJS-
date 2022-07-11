4b58f3f1e82acfcc0c0c527b3d6c9952e9b47f14
Максим Сергеев <msergeev@MacBook-Pro-Maksim.local>
2022-07-11 12:01:24 +0300

    удален неиспользуемый код

--- lesson_7/yoga/js/script.js
+++ lesson_7/yoga/js/script.js
@@ -116,7 +116,7 @@ const openModal = () => {
 
 openModal();
 
-// //form
+//form send
 let message = {
     loading: 'Загрузка...',
     success: 'Спасибо! Скоро мы с Вами свяжемся!',
@@ -127,40 +127,6 @@ let form = document.querySelector('.main-form');
 let contactsForm = document.querySelector('form');
 
 let input = document.getElementsByTagName('input');
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     form.appendChild(statusMessage);
-
-//     let request = new XMLHttpRequest();
-//     request.open('POST', 'server.php');
-//     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-
-//     let formData = new FormData(form);
-//     let obj = {};
-//     formData.forEach((value, key) => {
-//         obj[key] = value;
-//     });
-//     console.log(obj);
-//     let json = JSON.stringify(obj)
-//     request.send(json);
-
-//     request.addEventListener('readystatechange', () => {
-//         if (request.readyState < 4) {
-//             statusMessage.innerHTML = message.loading;
-//         } else if (request.readyState === 4 && request.status === 200) {
-//             statusMessage.innerHTML = message.success;
-//         } else {
-//             statusMessage.innerHTML = message.error;
-//         }
-//     })
-
-//     for (let i = 0; i < input.length; i++) {
-//         input[i].value = ''
-//     }
-// });
-
-//contacts-form
 
 let statusMessage = document.createElement('div');
 statusMessage.classList.add('satus');
